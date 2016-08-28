0cad3c4e66228edd2cc07fb6dd3d97ce4ce30789
martinmcbride <martin.mcbride@axlesoft.com>
2016-08-28 23:35:18 +0100

    Add ramp generator

--- pysound/envelopes.py
+++ pysound/envelopes.py
@@ -9,6 +9,20 @@ from pysound.buffer import get_buffer
 
 
 #
+# Multiply the source signal by a ramp envelope.
+# The envelope increases from the start value to the end value over the duration of the signal.
+#
+def ramp(rate=11025, duration=1, source=1, start=0, end=1):
+    samples = int(duration*rate)
+    data = np.zeros(samples)
+    d_source = get_buffer(samples, source)
+    for i in range(samples):
+        t = i/samples
+        data[i] = d_source[i]*(start + (end-start)*t)
+    return data
+
+
+#
 # Multiply the source signal by an attack/decay envelope.
 # The envelope increases from 0 to 1 for the attack duration, then falls
 # back to zero over the remainder of the time.
