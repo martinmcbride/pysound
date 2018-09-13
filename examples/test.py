d8d2c6ba02add9ceb6b51709662ff306afab3796
Martin <mcbride.martin@gmail.com>
2018-09-13 22:17:04 +0100

    Add table wave and sine wave

--- examples/test.py
+++ examples/test.py
@@ -5,6 +5,5 @@ from quack import soundfile
 from quack import oscillators
 
 params = buffer.BufferParams().time(2)
-ratio = oscillators.saw_wave(params, frequency=10, amplitude=0.4, offset=0.5)
-data = oscillators.saw_wave(params, frequency=400, ratio=ratio)
+data = oscillators.table_wave(params, frequency=400)
 file = soundfile.save(params, '/tmp/test.wav', data)
--- quack/oscillators.py
+++ quack/oscillators.py
@@ -11,11 +11,9 @@ from quack.buffer import create_buffer
 SINE_TABLE = None
 SINE_TABLE_SIZE = 16386
 
-def create_sine_table():
-    global SINE_TABLE
-    if SINE_TABLE is None:
-        args = np.linspace(0.0, 2*math.pi, num=SINE_TABLE_SIZE, endpoint=False)
-        SINE_TABLE = np.sin(args)
+def create_sine_table(size):
+    args = np.linspace(0.0, 2*math.pi, num=size, endpoint=False)
+    return np.sin(args)
 
 
 def square_wave(params, frequency=400, amplitude=1,
@@ -29,7 +27,6 @@ def square_wave(params, frequency=400, amplitude=1,
     :param ratio: ratio of high to low time (array or value)
     :return: array of resulting signal
     '''
-    t = 0
     frequency = create_buffer(params, frequency)
     amplitude = create_buffer(params, amplitude)
     offset = create_buffer(params, offset)
@@ -50,7 +47,6 @@ def saw_wave(params, frequency=400, amplitude=1,
     :param ratio: ratio of rise to fall time (array or value)
     :return: array of resulting signal
     '''
-    t = 0
     frequency = create_buffer(params, frequency)
     amplitude = create_buffer(params, amplitude)
     offset = create_buffer(params, offset)
@@ -59,39 +55,36 @@ def saw_wave(params, frequency=400, amplitude=1,
     output = offset + amplitude * np.where(phase < ratio, -1 + 2*phase/ratio, 1 - 2*(phase-ratio)/(1-ratio))
     return output
 
-##def saw_wave(settings=None, frequency=const(400), amplitude=const(1),
-##             offset=const(0), ratio=const(0.5)):
-##    '''
-##    Generate a saw wave (infinite sequence)
-##    rate - sample rate
-##    frequency - wave frequency (iterable)
-##    amplitude - wave amplitude (iterable)
-##    offset - offset of wave mean from zeor (iterable)
-##    ratio - ratio of rise to fall time (iterable)
-##    '''
-##    settings = get_settings(settings)
-##    t = 0
-##    for f, a, o, r in zip(frequency, amplitude, offset, ratio):
-##        t += f/settings.sample_rate
-##        t %= 1
-##        if t < r:
-##            v = -1 + 2*t/r
-##        else:
-##            v = 1 - 2*(t-r)/(1-r)
-##        yield o + a*v
-##        
-##def sine_wave(settings=None, frequency=const(400), amplitude=const(1), offset=const(0)):
-##    '''
-##    Generate a sine wave (infinite sequence)
-##    rate - sample rate
-##    frequency - wave frequency (iterable)
-##    amplitude - wave amplitude (iterable)
-##    offset - offset of wave mean from zeor (iterable)
-##    '''
-##    create_sine_table()
-##    settings = get_settings(settings)
-##    t = 0
-##    for f, a, o in zip(frequency, amplitude, offset):
-##        t += f/settings.sample_rate
-##        t %= 1
-##        yield o + a*SINE_TABLE[int(t*SINE_TABLE_SIZE)%SINE_TABLE_SIZE]
+def table_wave(params, frequency=400, amplitude=1,
+                offset=0, table=None):
+    '''
+    Generate a wave from a wavetable
+    :param params: buffer parameters, controls length of signal created
+    :param frequency: wave frequency (array or value)
+    :param amplitude: wave amplitude (array or value)
+    :param offset: offset of wave mean from zero (array or value)
+    :param table: table of values representing one full cycle of the waveform. Nominally the values
+     should be between +/-1 (array). The array can be any size.
+    :return: array of resulting signal
+    '''
+    if not table:
+        table = create_sine_table(65536)
+    size = table.size
+    frequency = create_buffer(params, frequency)
+    amplitude = create_buffer(params, amplitude)
+    offset = create_buffer(params, offset)
+    index = np.floor((np.add.accumulate(frequency / params.sample_rate) % 1) * size).astype(int)
+    output = offset + amplitude * table[index]
+    return output
+
+def sine_wave(params, frequency=400, amplitude=1, offset=0):
+    '''
+    Generate a sine wave
+    Convenience function, table_wave generates a sine wave by default
+    :param params: buffer parameters, controls length of signal created
+    :param frequency: wave frequency (array or value)
+    :param amplitude: wave amplitude (array or value)
+    :param offset: offset of wave mean from zero (array or value)
+    :return: array of resulting signal
+    '''
+    return table_wave(params, frequency, amplitude, offset)
