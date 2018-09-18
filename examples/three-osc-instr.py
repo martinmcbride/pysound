ca3ad6c8e478eebb3ff827c49538efa065d01432
Martin <mcbride.martin@gmail.com>
2018-09-18 18:14:10 +0100

    Add three osc example

---
+++ examples/three-osc-instr.py
@@ -0,0 +1,19 @@
+from quack import buffer
+from quack import soundfile
+from quack import oscillators
+from quack import envelopes
+from quack import mixers
+from quack import const
+
+def three_osc_instr(params, frequency, detune1, detune2):
+    tone1 = oscillators.saw_wave(params, frequency=frequency)
+    tone2 = oscillators.saw_wave(params, frequency=frequency*(1-detune1))
+    tone3 = oscillators.saw_wave(params, frequency=frequency*(1+detune2))
+    return (tone1 + tone2 + tone3) / 3
+
+
+params = buffer.BufferParams().set_time(4).set_tempo(120)
+detune1 = envelopes.linseg(params, 0.001, 0.007)
+detune2 = envelopes.linseg(params, 0.005, 0.002)
+data = three_osc_instr(params, frequency=400, detune1=detune1, detune2=detune2)
+soundfile.save(params, '/tmp/test.wav', data)
