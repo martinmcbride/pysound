6396b68aa40b3e22f374d6b1ed3b34790ac2d3af
Martin <mcbride.martin@gmail.com>
2018-09-21 20:47:08 +0100

    Rename to pysound

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# quack
+# PySound
 
 A Python library for creating digital sound and music.
 
-[Main website](http://www.martinmcbride.org/Quack_tutorial)
+[Main website](http://www.martinmcbride.org/pysound_tutorial)
--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
  
-setup(name='quack',
+setup(name='pysound',
       version='0.0',
-      url='https://github.com/martinmcbride/quack',
+      url='https://github.com/martinmcbride/pysound',
       license='MIT',
       author='Martin McBride',
       author_email='mcbride.martin@gmail.com',
-      description='Sound synthesiser',
+      description='Sound and music synthesiser',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       packages=find_packages(exclude=['examples']),
       long_description=open('README.md').read(),
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
       setup_requires=[])
