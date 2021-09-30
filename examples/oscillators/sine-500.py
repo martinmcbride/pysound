a2d22fbef24db29b947f70b5c27a94d9475e7eaf
Martin <mcbride.martin@gmail.com>
2021-09-30 19:27:00 +0100

    Change example sine wave to 500 Hz

--- examples/oscillators/sine-500.py
+++ examples/oscillators/sine-500.py
@@ -9,13 +9,16 @@ from pysound import soundfile
 from pysound.const import Notes
 from pysound.graphs import Plotter
 
-## Create a sine wave middle C
+## Create a sine wave at 500 Hz
 params = BufferParams()
-out = sine_wave(params, frequency=Notes.C4)
-soundfile.save(params, '/tmp/sine-c4.wav', out)
+out = sine_wave(params, frequency=500)
+soundfile.save(params, '/tmp/sine-500.wav', out)
 
 ## Plot a graph of the first part of the previous sine wave
-Plotter(params, 'sine-c4.png', out).with_timerange(0, 0.1).plot()
+Plotter(params, 'sine-500.png', out).with_timerange(0, 0.01)\
+                                    .with_title("500 Hz sine wave")\
+                                    .in_milliseconds()\
+                                    .plot()
 
 
 
