6f829df10f2e0f7e870d87679ee54b0b4bcde871
Martin <mcbride.martin@gmail.com>
2024-08-31 17:49:57 +0100

    GenericEnvelopeUnit with tests

--- pysound/env_units.py
+++ pysound/env_units.py
@@ -1,25 +1,54 @@
- # Author:  Martin McBride
+import math  # Author:  Martin McBride
 # Created: 2024-08-19
 # Copyright (C) 2024, Martin McBride
 # License: MIT
 
 import numpy as np
 
-from pysound_defs import Unit, DATA_LENGTH, Connection, TABLE_SIZE, DATA_TYPE, PARAMS, BaseUnit
+from pysound.pysound_defs import Unit, DATA_LENGTH, Connection, TABLE_SIZE, DATA_TYPE, PARAMS, BaseUnit
 
 
 
 class GenericEnvelopeUnit(BaseUnit):
 
     def __init__(self, sections: list, amplitude: Unit=1):
+        """
+        sections is a list of evelope sections. Each section is a tuple:
+
+        (l, a) creates a constant level a for l samples
+        (l, a, b) creates a linear ramp from a to b over l samples
+        (l, a, b, f) creates an exponential ramp from a to b with curve factor f over l samples
+        """
         super().__init__()
-        self.sections = sections
+        self.sections = []
+        for s in sections:
+            if len(s) == 2:
+                self.sections.append((s[0], s[1], s[1], 0))
+            elif len(s) == 3:
+                self.sections.append((s[0], s[1], s[2], 0))
+            elif len(s) == 4:
+                self.sections.append(tuple(s))
+            else:
+                ValueError("Each section must be a sequence of 2, 3, or 4 values")
         self.amplitude = Connection(amplitude)
         self.current_section = 0
 
 
     def get(self):
-        a = self.amplitude.get(self.data_length)
-        out = None
+        if self.current_section >= len(self.sections):
+            return np.zeros(DATA_LENGTH, dtype=DATA_TYPE)
+
+        section = self.sections[self.current_section]
+        self.current_section += 1
+        x0 = 0
+        x1, y0, y1, factor = section
+
+        a = self.amplitude.get(x1)
+
+        if abs(factor) < 0.00001:
+            out = np.linspace(y0, y1, num=x1, endpoint=False, dtype=DATA_TYPE)
+        else:
+            xvals = np.linspace(0, x1, num=x1, endpoint=False, dtype=DATA_TYPE) / x1
+            out = y0 + (y1 - y0) * (1 - np.exp(-factor * xvals)) / (1 - math.exp(-factor))
 
         return out
--- test/generate_expected_results.py
+++ test/generate_expected_results.py
@@ -1,5 +1,5 @@
 # Generate expected results file, and a plot, for any unit
-
+from pysound.env_units import GenericEnvelopeUnit
 from pysound.graph import Plotter
 from pysound.osc_units import SineUnit, SquareUnit, SawUnit
 from pysound.pysound_defs import PARAMS
@@ -7,9 +7,9 @@ from pysound.sound_file import write_text_file
 
 
 def get_unit():
-    return SawUnit()
+    return GenericEnvelopeUnit([(441, 0.5, -0.6, -5)])
 
-DATA_FILE = "TestSawUnit_test_defaults.txt"
+DATA_FILE = "TesTGenericEnvelopeUnit_test_negexp.txt"
 
 DATA_LENGTH = 441
 
